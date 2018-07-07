0fd44646cd451336fc096eb3ea77624deccb25dc
Matthew Messier <messier_matthew@yahoo.com>
2018-07-07 11:56:20 -0400

    adding scripts in package json and created the base for the server

---
+++ server.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let app = express();
+
+let corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");    
+  res.header("Access-Control-Allow-Headers", "http://localhost:4200");
+  next();
+});
+
+module.exports = app;
+
+app.listen(8080);
+process.stdout.write('Welcome to your game.\nPlease enter your userId:');
